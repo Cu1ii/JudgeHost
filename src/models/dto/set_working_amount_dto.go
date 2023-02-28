a4b5432ef678a57fecf51654273284ed2716e483
Cu1 <cu1universe@163.com>
2023-02-28 14:10:51 +0800

    update: 完成测试

--- src/models/dto/set_working_amount_dto.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type SetWorkingAmountDTO struct {
-	MaxWorkingAmount int  `form:"max_work_amount" json:"max_work_amount" validate:"required"`
-	ForceSet         bool `form:"force_set" json:"force_set"`
-}
